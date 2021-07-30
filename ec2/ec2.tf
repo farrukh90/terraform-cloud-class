a4cfe7d872a0776a2b7d3aac598c4df3ad28cf04
Farrukh Sadykov <farrukhsadykov@gmail.com>
2021-07-29 19:34:54 -0500

    Update ec2.tf

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -22,3 +22,18 @@ resource "aws_instance" "web" {
     Name = "HelloWorld"
   }
 }
+
+
+
+resource "aws_instance" "web2" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "NewOne"
+  }
+}
+
+
+
+
